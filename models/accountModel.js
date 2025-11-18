a0db2f7b616060cf355737186b023684f0c9f551
Ei Shin Thant <S10269490@connect.np.edu.sg>
2025-11-18 16:12:08 +0800

    accountModel.js

--- models/accountModel.js
+++ models/accountModel.js
@@ -47,7 +47,52 @@ async function getAccountsByUserId(userId) {
         if (pool) pool.close();
     }
 }
+
+
+async function getBalance(accountNo) {
+    let connection;
+    try {
+        connection = await sql.connect(dbConfig);
+        const result = await connection.request()
+            .input("accountNo", sql.Int, accountNo)
+            .query(`SELECT balance FROM Accounts WHERE accountNo = @accountNo`);
+                
+        if (!result.recordset.length) {
+        return null;
+        }
+        
+        return result.recordset[0].balance;
+    }catch (err) {
+        console.error("DB Error (getBalance):", err.message);
+        throw err;
+    }finally {   
+        if (connection) await connection.close();
+    }
+}
+
+async function updateBalance(accountNo, amount) {
+    let connection;
+    try {
+        connection = await sql.connect(dbConfig);   
+        const result = await connection.request()
+            .input("accountNo", sql.Int, accountNo)
+            .input("amount", sql.Decimal(18, 2), amount)
+            .query(`
+                UPDATE Accounts 
+                SET balance = balance - @amount
+                WHERE accountNo = @accountNo
+            `);
+
+        return result.rowsAffected[0] === 1;
+    } catch (err) {
+        console.error("DB Error (updateBalance):", err.message);
+        throw err;
+    } finally {
+        if (connection) await connection.close();
+    }   
+}
+
 module.exports = {
     createAccount,
-    getAccountsByUserId
+    getAccountsByUserId,getBalance, updateBalance
 };
